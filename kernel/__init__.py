97ece9e3144a2a8e566174f415c52259a5ac3908
晚大安 <1003028440@qq.com>
2026-08-19 22:34:01 +0800

    aic-v0.1.3版本发布

--- kernel/__init__.py
+++ kernel/__init__.py
@@ -17,7 +17,7 @@ from .kernel import (
 from .layout import check_shell_content, check_shell_layout
 from .plugin import Plugin
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 __all__ = [
     "Context",
